ffe12d187280d45bff30a7d245a5f51822ca1827
harukaty04 <haruka.kimura04@gmail.com>
2020-11-17 22:51:54 +0900

    create header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -11,3 +11,23 @@
     </div>
   </header>
 </template>
+
+<style lang="stylus" scoped>
+header
+  position fixed
+  top 0
+  width 100%
+  text-align center
+  padding 10px
+  h1
+    width fit-content
+    margin 0 auto
+    font-size 1.4rem
+  .btns
+    position absolute
+    top 10px
+    right 30px
+    cursor pointer
+    img
+      width 100%
+</style>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,24 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+//使いたいアイコンを登録するためのlibraryをインポート
+import { library } from '@fortawesome/fontawesome-svg-core'
+//今回使用する３つのアイコンをインポート
+import {
+  faUser,
+  faSignOutAlt,
+  faEllipsisV
+} from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+//インポートしたアイコンをlibraryに登録
+library.add(
+  faUser,
+  faSignOutAlt,
+  faEllipsisV
+)
+//アイコンを表示するためにFontAwesomeIconをインポートしfaで呼べるよう設定
+Vue.component('fa', FontAwesomeIcon)
+
 Vue.config.productionTip = false
 
 new Vue({
