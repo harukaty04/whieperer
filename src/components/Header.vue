d7342d9571e75b43a376f50bc63a060235f2b13a
harukaty04 <haruka.kimura04@gmail.com>
2020-11-17 22:25:39 +0900

    create Header.vue

---
+++ src/components/Header.vue
@@ -0,0 +1,13 @@
+<!-- Header.vue -->
+<template>
+  <header>
+    <router-link to="/">
+      <h1>whisp.</h1>
+    </router-link>
+    <div class="btns">
+      <button>
+        <fa icon="user" />
+      </button>
+    </div>
+  </header>
+</template>
