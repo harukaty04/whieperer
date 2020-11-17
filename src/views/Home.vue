ef5cce1dfc0210bcd54dbc3f06f1123921f3959c
harukaty04 <haruka.kimura04@gmail.com>
2020-11-17 23:56:50 +0900

    componentization list

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,45 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  
+    <div class="home">
+    <div class="list">
+  <Item 
+        v-for="whisper in orderBy(whispers,'date',-1)"
+        :key="whisper.id"
+        :id="whisper.id"
+        :uid="whisper.uid" 
+      />
+    </div> 
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Item from '@/components/Item.vue'
+import { db } from '../main'
+
+//vuew-filterを使用するために追加
+import Vue2Filters from 'vue2-filters'
 
 export default {
-  name: 'Home',
+  name: 'home',
   components: {
-    HelloWorld
-  }
+    
+  },
+  data () {
+    return {
+      whispers: []
+    }
+  },
+  firestore () {
+    return {
+      whispers: db.collection('whispers')
+    }
+  },  
+  components: {
+    Item
+  },
+  mixins: [Vue2Filters.mixin]
 }
 </script>
+
+<style lang="stylus"scoped>
