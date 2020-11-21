e9f070b01e7776665287b2ed9162938c8465fc37
harukaty04 <haruka.kimura04@gmail.com>
2020-11-21 20:36:08 +0900

    display whiperer list

--- src/views/User.vue
+++ src/views/User.vue
@@ -2,18 +2,28 @@
 
 <script>
 import { db } from '../main'
+import firebase from 'firebase'
+import Item from '@/components/Item' 
+import Vue2Filters from 'vue2-filters' 
+
 
 export default {
+    components: {
+    Item
+  },
   data () {
     return {
       user: {}
+      myWhispers: [] 
     }
   },
   firestore () {
     return {
       user: db.collection('users').doc(this.$route.params.uid)
+      myWhispers: db.collection('whispers').where('uid','==',this.$route.params.uid) 
     }
-  }
+  },
+  mixins: [Vue2Filters.mixin]
 }
 </script>
 
@@ -23,8 +33,19 @@ export default {
       <div class="avatar":style="'background-image: url('+user.photoURL+')'"></div>
       <div class="texts">
         <h1>{{user.name}}</h1>
+        <p v-if="myWhispers.length > 1">{{myWhispers.length}} whispers</p>
+        <p v-else>{{myWhispers.length}} whisper</p>
       </div>
     </div>
+    <div class="list">
+      <Item 
+        v-for="whisper in orderBy(myWhispers,'date',-1)"
+        :key="whisper.id" 
+        :id="whisper.id"
+        :uid="whisper.uid" 
+      />
+    </div>
+    
   </div>
 </template>
 
